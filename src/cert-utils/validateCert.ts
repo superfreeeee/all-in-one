8049cdf7c2b9ce79146f4be37b04c75d1dd9759c
superfree <superfreeeee@gmail.com>
2023-11-27 22:48:26 +0800

    update validateCert comments

--- src/cert-utils/validateCert.ts
+++ src/cert-utils/validateCert.ts
@@ -1,6 +1,11 @@
 import crypto from 'crypto';
 import { CertificatePemInfo } from './types';
 
+/**
+ * Validate certificate with publicKey & privateKey pair
+ * @param param0
+ * @returns
+ */
 export const validateCert = ({ cert, key }: CertificatePemInfo) => {
   const TEST_STR = 'hello world';
   const encrypted = crypto.publicEncrypt(cert, Buffer.from(TEST_STR));
